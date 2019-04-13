82614d74b1017e5f46046913cc8676df58cc8060
kyuubi <duarte.garin@gmail.com>
2019-04-13 17:07:39 +1000

    Updating active trail on de-selection

--- js/tree.js
+++ js/tree.js
@@ -83,7 +83,9 @@
         data.tree.generateFormElements(name + '[]');
         // When a selection is made, we need to update the active trail.
         if(data.node.selected){
-          data.node.parent.addClass('activeTrail');
+          data.node.visitParents(function (node) {
+            node.addClass('activeTrail');
+          });
         }
         else if(data.node.getSelectedNodes().length === 0){
           data.node.visitParents(function (node) {
