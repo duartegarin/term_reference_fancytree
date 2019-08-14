eedddc0c2549b5e03c29da7719f5293e78510ff3
kyuubi <duarte.garin@gmail.com>
2019-08-14 17:28:38 +1000

    Fixing critical bug where tree isn't expanding.

--- src/Element/TermReferenceFancytree.php
+++ src/Element/TermReferenceFancytree.php
@@ -38,7 +38,15 @@ class TermReferenceFancytree extends FormElement {
     if (!empty($element['#vocabulary'])) {
 
       // Get the ancestors of the selected items.
-      $ancestors = TermReferenceFancytree::getSelectedAncestors($element['#default_value']);
+      // If we are processing input (submit) we want to pass the state with selected items.
+      if($form_state->isProcessingInput()){
+        $ancestors = TermReferenceFancytree::getSelectedAncestors($form_state->getUserInput()[$element['#field_name']], TRUE);
+      }
+      // If we are not we want the default values (previously submitted).
+      else{
+        $ancestors = TermReferenceFancytree::getSelectedAncestors($element['#default_value'], FALSE);
+      }
+
       // Build a list of top level nodes, including children if containing
       // selected items.
       $list = TermReferenceFancytree::getTopLevelNodes($element, $ancestors, $form_state);
@@ -63,20 +71,27 @@ class TermReferenceFancytree extends FormElement {
   /**
    * Function that goes through the default values and obtains their ancestors.
    *
-   * @param array $default_values
+   * @param array $values
    *   The selected items.
    *
    * @return array
    *   The list of ancestors.
    */
-  public static function getSelectedAncestors(array $default_values) {
+  public static function getSelectedAncestors(array $values, $processing_input) {
 
     $all_ancestors = [];
 
-    foreach ($default_values as $default_value) {
+    foreach ($values as $value) {
+
+      // Check if we are processing input or not because the structure of
+      // default values and form state differs.
+      if(!$processing_input){
+        $value = $value['target_id'];
+      }
+
       $term_ancestors = \Drupal::entityTypeManager()
         ->getStorage('taxonomy_term')
-        ->loadAllParents($default_value['target_id']);
+        ->loadAllParents($value);
       foreach ($term_ancestors as $ancestor) {
         $all_ancestors[$ancestor->id()] = $ancestor;
       }
@@ -212,7 +227,7 @@ class TermReferenceFancytree extends FormElement {
   /**
    * Function that generates the nested list for the JSON array structure.
    */
-  public static function getNestedListJsonArray($terms, $element, $ancestors = NULL, $form_state) {
+  public static function getNestedListJsonArray($terms, $element, $ancestors = NULL, $form_state = NULL) {
     $items = [];
     if (!empty($terms)) {
       foreach ($terms as $term) {
@@ -223,7 +238,7 @@ class TermReferenceFancytree extends FormElement {
 
         // Checking the term against the form state and default values and if present, mark as
         // selected.
-        if($form_state->getUserInput()){
+        if($form_state && $form_state->getUserInput()){
           if(in_array($term->id(), $form_state->getValues()[$element['#field_name']])){
             $item['selected'] = TRUE;
           }
