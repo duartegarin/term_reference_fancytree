e8faed51b080fececcb5fff2ca19af3a73888aeb
Ivo <ivo@vangeertruyen.be>
2019-10-07 11:10:07 +0200

    Add widget configuration setting for selection mode.

--- src/Plugin/Field/FieldWidget/TreeWidget.php
+++ src/Plugin/Field/FieldWidget/TreeWidget.php
@@ -37,8 +37,39 @@ class TreeWidget extends WidgetBase implements WidgetInterface {
     $element['#type'] = 'term_reference_fancytree';
     $element['#default_value'] = $items->getValue();
     $element['#vocabulary'] = $vocabularies;
+    $element['#selection_mode'] = $this->getSetting('selection_mode');
 
     return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+        'selection_mode' => '2',
+      ] + parent::defaultSettings();
+  }
+
+
+  /**
+   * {@inheritDoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form = parent::settingsForm($form, $form_state);
+
+    $form['selection_mode'] = [
+      '#title' => t('Selection mode'),
+      '#default_value' => $this->getSetting('selection_mode'),
+      '#type' => 'radios',
+      '#options' => [
+        '1' => t('single selection'),
+        '2' => t('multiple selection'),
+        '3' => t('hierarchical selection'),
+      ],
+    ];
+
+    return $form;
   }
 
 }
